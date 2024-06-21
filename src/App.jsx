b80a2c493924de933937aee8186cc2692fe6f322
annafricks <annafricks33@gmail.com>
2024-06-21 16:53:09 -0400

    footer- needs polishing ✨

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import React from 'react';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from "./components/Footer";
+import TerraHomeCard from './components/TerraHomeCard';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-neutral-50 h-[100vh] center'>Hello World</div>
-  )
+    <div>
+    <TerraHomeCard />
+    <Footer />
+</div>
+  );
 }
 
-export default App
+export default App;
