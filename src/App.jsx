1fb7e539ab75f27dcd30ba231deeab298b2b523a
annafricks <annafricks33@gmail.com>
2024-06-24 16:52:00 -0400

    fix conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-
-import './App.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Homepage from './pages/Homepage'
-// import PlanList from './pages/PlanList'
-import PlanDetails from './pages/PlanDetails'
-import About from './pages/About'
-import SignupPage from './pages/SignupPage'
-import LoginPage from './pages/LoginPage'
-import Error from './pages/Error'
-import React from 'react';
-import { useState } from 'react'
-import { PlanContext } from './context/plan.context';
-import TerraHomeCard from './components/TerraHomeCard';
-import { Navigate, Outlet, Route, Routes } from "react-router-dom";
-import { Toaster } from 'react-hot-toast';
-
-=======
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
@@ -29,7 +9,7 @@ import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import Error from "./pages/Error";
 import React from "react";
->>>>>>> 200c9acf8ccd86d2c68f09a5f0c0f13eb79e4b7e
+import TerraHomeCard from "./components/TerraHomeCard";
 
 function App() {
   return (
@@ -37,17 +17,6 @@ function App() {
       <Navbar />
       <TerraHomeCard />
       <Routes>
-<<<<<<< HEAD
-      <Route path="/" element={<Homepage/>} />
-      {/* <Route path="/destinations" element={<PlanList />} /> */}
-      <Route path="/destinations/:planId" element={<PlanDetails />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/*" element={<Error />} />
-      </Routes>
-    <Footer />
-=======
         <Route path="/" element={<Homepage />} />
         <Route path="/plan" element={<PlanList />} />
         <Route path="/plan/:planId" element={<PlanDetails />} />
@@ -56,7 +25,6 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/*" element={<Error />} />
       </Routes>
->>>>>>> 200c9acf8ccd86d2c68f09a5f0c0f13eb79e4b7e
     </div>
   );
 }
