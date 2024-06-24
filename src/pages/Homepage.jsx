9b91c98e8af0f3ee61085b7e39286fa23bd3f195
annafricks <annafricks33@gmail.com>
2024-06-24 14:46:46 -0400

    homepage, needs addtl work

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,14 +3,16 @@ import Footer from '../components/Footer'
 
 function Homepage() {
   return (
-    <div>
-        <h1></h1>
+    <div className='flex flex-col items-center justify-center h-screen'>
+        <h1 className='text-3xl font-bold mb-4'>Who we are..</h1>
+        <p className='text-center'>
+             About us.... Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, odio?</p>
         <Footer />
         
 
     </div>
 
-  )
+  );
 }
 
 export default Homepage
